0f1e12a33ac05ad6c2d1c3fec459633366e25027
Kilian Northoff <kilian@northoff-online.de>
2023-04-18 21:31:17 +0200

    cpu renderer

--- src/main.rs
+++ src/main.rs
@@ -1,199 +1,214 @@
-use ocl::ProQue;
+use rayon::prelude::*;
+use rug::ops::CompleteRound;
+use rug::{Complex, Float};
 use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::mouse::{MouseButton, MouseWheelDirection};
-use sdl2::pixels::{Color, PixelFormatEnum};
+use sdl2::mouse::MouseButton;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
 use sdl2::surface::Surface;
+use std::error::Error;
+use std::sync::mpsc::Receiver;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-const SIZE: usize = 2000;
 const TICK_DURATION: Duration = Duration::new(0, 1_000_000_000u32 / 60);
 
-fn create_pro_que() -> ProQue {
-    let src = r#"
-        typedef struct {
-            float real;
-            float imag;
-        } Complex;
+fn mandelbrot(x: Float, y: Float, precision: u32) -> [u8; 3] {
+    let mut z = Complex::new(precision);
+    let c = Complex::with_val(precision, (x, y));
 
-        inline void complex_add(Complex* c1, Complex* c2) {
-            c1->real += c2->real;
-            c1->imag += c2->imag;
+    let mut color = [0, 0, 0];
+    for i in 0..255 {
+        z *= z.clone();
+        z += c.clone();
+
+        if (z.real() * z.real() + z.imag() * z.imag()).complete(precision) > 4.0 {
+            let co = 255 - i;
+            color[0] = co as u8;
+            color[1] = (co * 2 + 150) as u8;
+            color[2] = (co * 3 + 60) as u8;
+            break;
         }
-
-        inline void complex_mul(Complex* c1, Complex* c2) {
-            float new_real = c1->real * c2->real - c1->imag * c2->imag;
-            float new_imag = c1->real * c2->imag + c1->imag * c2->real;
-            c1->real = new_real;
-            c1->imag = new_imag;
-        }
-
-        __kernel void mandelbrot(
-                __global char* buffer,
-                int size,
-                float center_x,
-                float center_y,
-                float width,
-                int iteration_num
-        ) {
-            int id = get_global_id(0);
-            int index = id / 3;
-
-            float pixel_x = index / size;
-            float pixel_y = index % size;
-
-            float x = center_x + width * (pixel_x / size - 0.5);
-            float y = center_y + width * (pixel_y / size - 0.5);
-
-            Complex z = { 0.0f, 0.0f };
-            Complex c = { x, y };
-
-            int r_color = 0x00;
-            int g_color = 0x00;
-            int b_color = 0x00;
-
-            for (int i = 0; i < iteration_num; i++) {
-                complex_mul(&z, &z);
-                complex_add(&z, &c);
-
-                if (z.real * z.real + z.imag * z.imag > 4.0) {
-                    int color = i; //255/100 * (i * 10);
-                    color = 255 - color;
-                    r_color = color;
-                    g_color = (color * 2 + 150) % 256;
-                    b_color = (color * 3 + 60) % 256;
-                    break;
-                }
-            }
-
-            if (id % 3 == 0) {
-                buffer[id] = r_color;
-            } else if (id % 3 == 1) {
-                buffer[id] = g_color;
-            } else {
-                buffer[id] = b_color;
-            }
-        }
-    "#;
-    ProQue::builder()
-        .src(src)
-        .dims(SIZE * SIZE * 3)
-        .build()
-        .expect("Error building the kernel")
-}
-
-fn calculate_values(pro_que: &ProQue, pos_x: f32, pos_y: f32, width: f32) -> Vec<u8> {
-    let buffer = pro_que.create_buffer::<u8>().unwrap();
-
-    let kernel = pro_que
-        .kernel_builder("mandelbrot")
-        .arg(&buffer)
-        .arg(SIZE as u32)
-        .arg(pos_x)
-        .arg(pos_y)
-        .arg(width)
-        .arg(1000)
-        .build()
-        .unwrap();
-
-    unsafe {
-        kernel.enq().unwrap();
     }
-
-    let mut result = vec![0; buffer.len()];
-    buffer.read(&mut result).enq().unwrap();
-    result
+    color
 }
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+fn create_image(
+    size: u32,
+    width: &Float,
+    x: &Float,
+    y: &Float,
+    precision: u32,
+) -> (Option<Receiver<Option<u32>>>, Receiver<Vec<u8>>) {
+    let width = Float::with_val(precision, width);
+    let center = (Float::with_val(precision, x), Float::with_val(precision, y));
+    let (progress_tx, progress_rx) = mpsc::channel();
+    let (res_tx, res_rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let progress_tx = Arc::new(Mutex::new(progress_tx));
+
+        let image = (0..size)
+            .collect::<Vec<_>>()
+            .par_iter()
+            .map(|&y| {
+                let res = (0..size)
+                    .map(|x| {
+                        let x = center.0.clone() + width.clone() * x / size;
+                        let y = center.1.clone() + width.clone() * y / size;
+                        mandelbrot(x, y, precision)
+                    })
+                    .collect::<Vec<_>>();
+                let p = progress_tx.lock().unwrap();
+                p.send(Some(y)).unwrap();
+                res
+            })
+            .flatten()
+            .flatten()
+            .collect::<Vec<_>>();
+        res_tx.send(image).unwrap();
+        let p = progress_tx.lock().unwrap();
+        p.send(None).unwrap()
+    });
+
+    (Some(progress_rx), res_rx)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let size = 1000;
 
     let window = video_subsystem
-        .window("Mandelbrot", SIZE as u32, SIZE as u32)
+        .window("Mandelbrot Viewer", size, size + 20)
         .position_centered()
-        .build()
-        .expect("could not initialize video subsystem");
+        .build()?;
 
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .expect("could not make a canvas");
+    let mut canvas = window.into_canvas().build()?;
 
-    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.set_draw_color((255, 255, 255));
     canvas.clear();
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut scale = 1.0;
-
-    let pro_que = create_pro_que();
-
     let mut positions = vec![];
-    for i in 0..(SIZE * SIZE) {
-        positions.push(((i % SIZE) as i32, (i / SIZE) as i32));
+    for i in 0..(size * size) {
+        positions.push(((i % size) as i32, (i / size) as i32));
     }
 
-    let mut center_x = 0.0f32;
-    let mut center_y = 0.0f32;
-    let mut width = 4.0f32;
+    let mut selection_start: Option<(i32, i32)> = None;
+    let mut selection_end: Option<(i32, i32)> = None;
+
+    let mut precision = 32;
+    let mut view_width = Float::with_val(precision, 4.0);
+    let mut top_left_x = Float::with_val(precision, -2.0);
+    let mut top_left_y = Float::with_val(precision, -2.0);
+
+    let mut current_progress = 0;
+    let (mut progress_rx, mut image_rx) =
+        create_image(size, &view_width, &top_left_x, &top_left_y, precision);
+
+    let mut image = vec![0; (size * size * 3) as usize];
+
+    let creator = canvas.texture_creator();
 
     'running: loop {
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
+        canvas.set_draw_color((0, 0, 0));
         canvas.clear();
 
-        let values = calculate_values(&pro_que, center_x, center_y, width);
-        let mut values = values;
-        let creator = canvas.texture_creator();
         let texture = Surface::from_data(
-            values.as_mut(),
-            SIZE as u32,
-            SIZE as u32,
-            (SIZE * 3) as u32,
+            &mut image,
+            size,
+            size,
+            (size * 3) as u32,
             PixelFormatEnum::RGB24,
-        )
-        .unwrap()
-        .as_texture(&creator)
-        .unwrap();
-        canvas.copy(&texture, None, None).unwrap();
+        )?
+        .as_texture(&creator)?;
+        canvas.copy(&texture, None, None)?;
 
-        scale *= 1.01;
+        canvas.set_draw_color((0, 255, 0));
+        canvas.fill_rect(Rect::new(0, size as i32, current_progress, 20))?;
 
-        let mut mouse_motion = (0, 0);
+        let mouse = event_pump.mouse_state();
+        if let Some(start) = selection_start {
+            let width = (mouse.x() - start.0).max(mouse.y() - start.1);
+            let end = selection_end.unwrap_or((start.0 + width, start.1 + width));
+
+            let r = Rect::new(
+                start.0.min(end.0),
+                start.1.min(end.1),
+                (end.0 - start.0).abs() as u32,
+                (end.1 - start.1).abs() as u32,
+            );
+            canvas.set_draw_color((255, 255, 255));
+            canvas.draw_rect(r)?;
+        }
+
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => {
+                Event::Quit { .. } => {
                     break 'running;
                 }
-                Event::MouseWheel { y, .. } => {
-                    if y == 1 {
-                        width *= 1.05;
-                    } else if y == -1 {
-                        width *= 0.95;
+                Event::MouseButtonDown {
+                    mouse_btn, x, y, ..
+                } => {
+                    if mouse_btn == MouseButton::Left && progress_rx.is_none() {
+                        if selection_start.is_none() {
+                            selection_start = Some((x, y))
+                        } else if selection_end.is_none() {
+                            if let Some(start) = selection_start {
+                                let width = (mouse.x() - start.0).max(mouse.y() - start.1);
+                                selection_end = Some((start.0 + width, start.1 + width));
+                            }
+                        } else {
+                            if let Some(start) = selection_start {
+                                if let Some(end) = selection_end {
+                                    selection_start = None;
+                                    selection_end = None;
+
+                                    precision += 1;
+                                    top_left_x.set_prec(precision);
+                                    top_left_y.set_prec(precision);
+                                    view_width.set_prec(precision);
+
+                                    top_left_x = top_left_x + view_width.clone() * start.0 / size;
+                                    top_left_y = top_left_y + view_width.clone() * start.1 / size;
+                                    view_width = view_width * (end.0 - start.0) / size;
+                                    (progress_rx, image_rx) = create_image(
+                                        size,
+                                        &view_width,
+                                        &top_left_x,
+                                        &top_left_y,
+                                        precision,
+                                    );
+                                }
+                            }
+                        }
+                    } else if mouse_btn == MouseButton::Right {
+                        selection_start = None;
+                        selection_end = None;
                     }
-                }
-                Event::MouseMotion { xrel, yrel, .. } => {
-                    mouse_motion.0 += xrel;
-                    mouse_motion.1 += yrel;
                 }
                 _ => {}
             }
         }
 
-        let mouse_state = event_pump.mouse_state();
-
-        if mouse_state.is_mouse_button_pressed(MouseButton::Left) {
-            center_x -= width * (mouse_motion.1 as f32) / (SIZE as f32);
-            center_y -= width * (mouse_motion.0 as f32) / (SIZE as f32);
+        if let Some(rx) = &progress_rx {
+            while let Ok(_) = rx.try_recv() {
+                current_progress += 1;
+            }
+            if let Ok(im) = image_rx.try_recv() {
+                image = im;
+                progress_rx = None;
+                current_progress = 0;
+            }
         }
 
         canvas.present();
         thread::sleep(TICK_DURATION);
     }
+    Ok(())
 }
